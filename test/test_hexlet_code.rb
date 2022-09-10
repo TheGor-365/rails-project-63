22787662fd484af7ee42ad5b630797ac8a761472
Gor <7495500@gmail.com>
2022-09-10 23:21:25 +0300

    Add tests for tag generation

--- test/test_hexlet_code.rb
+++ test/test_hexlet_code.rb
@@ -1,13 +1,33 @@
 # frozen_string_literal: true
 
-require "test_helper"
+require_relative "test_helper"
 
 class TestHexletCode < Minitest::Test
-  def test_that_it_has_a_version_number
-    refute_nil ::HexletCode::VERSION
+  def test_tag_without_attributes_build
+    paired_tag = "<form></form>"
+    unpaired_tag = "<area>"
+
+    assert { HexletCode::Tag.build('form') == paired_tag }
+    assert { HexletCode::Tag.build('area') == unpaired_tag }
   end
 
-  def test_it_does_something_useful
-    assert false
+  def test_tag_with_attributes_build
+    paired_tag = "<form action='action_page/:id' method='get'></form>"
+    unpaired_tag = "<area src='workplace.jpg' alt='Workplace'>"
+
+    assert { HexletCode::Tag.build('form', action: "action_page/:id", method: "get") == paired_tag }
+    assert { HexletCode::Tag.build('area', src: "workplace.jpg", alt: "Workplace") == unpaired_tag }
+  end
+
+  def test_tag_with_blocks_build
+    paired_tag = "<label>Email</label>"
+
+    assert { HexletCode::Tag.build('label') { 'Email' } == paired_tag }
+  end
+
+  def test_tag_with_blocks_and_attributes_build
+    paired_tag = "<label for='email'>Email</label>"
+
+    assert { HexletCode::Tag.build('label', for: 'email') { 'Email' } == paired_tag }
   end
 end
