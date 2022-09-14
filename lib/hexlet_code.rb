dde71daaea6c5af21aaea46342f8073874c6d295
Gor <7495500@gmail.com>
2022-09-14 18:57:45 +0300

    Add tests for form generater

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,7 +1,17 @@
 # frozen_string_literal: true
 
 require_relative "hexlet_code/version"
-
+# generates HTML forms
 module HexletCode
   autoload(:Tag, "./lib/hexlet_code/tag.rb")
+
+  class << self
+    def form_for(_action, job = {})
+      return "<form action='#' method='post'></form>" if job.size.zero?
+
+      job.each_pair do |_name, value|
+        return "<form action='#{value}' method='post'></form>" if job.key?(:url)
+      end
+    end
+  end
 end
