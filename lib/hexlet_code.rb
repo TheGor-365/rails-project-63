b1549af488501424274d71cdcae3cbeae4f99a76
Gor <7495500@gmail.com>
2024-01-21 12:03:46 +0300

    Fix linter issues

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -1,18 +1,18 @@
 # frozen_string_literal: true
 
-require_relative 'hexlet_code/version'
+require_relative "hexlet_code/version"
 # require_relative 'hexlet_code/struct'
 
-require 'active_support/all'
+require "active_support/all"
 
 # generates HTML forms
 module HexletCode
-  autoload(:Tag, './lib/hexlet_code/tag.rb')
+  autoload(:Tag, "./lib/hexlet_code/tag.rb")
 
   def self.form_for(struct, url = {}, *form)
     form << (url.key?(:url) ? "<form action='#{url.fetch(:url)}' method='post'>\n" : "<form action='#' method='post'>\n")
     form << yield(struct)
-    form << '</form>'
+    form << "</form>"
     form.join
   end
 
@@ -22,6 +22,7 @@ module HexletCode
   end
 end
 
+# generates HTML fields for form
 class Struct
   include HexletCode
 
@@ -31,10 +32,10 @@ class Struct
 
   def input(attr_name, **options)
     @field = @attributes.each_with_object({}) do |(name, value), pair|
-      case options[:as]
-      when :text then pair[name] = "name='#{name}' cols='#{options.fetch(:cols, 20)}' rows='#{options.fetch(:rows, 40)}'"
-      else pair[name] = "name='#{name}' type='text' value='#{value}'"
-      end
+      pair[name] = case options[:as]
+                   when :text then "name='#{name}' cols='#{options.fetch(:cols, 20)}' rows='#{options.fetch(:rows, 40)}'"
+                   else "name='#{name}' type='text' value='#{value}'"
+                   end
     end
 
     field_builder(attr_name, **options)
@@ -46,14 +47,14 @@ class Struct
     case options[:as]
     when :text
       @fields << label(attr_name)
-      @fields << '  <textarea '
+      @fields << "  <textarea "
       @fields << @field.fetch(attr_name)
-      @fields << '>'
+      @fields << ">"
       @fields << @attributes.fetch(attr_name)
       @fields << "</textarea>\n"
     else
       @fields << label(attr_name)
-      @fields << '  <input '
+      @fields << "  <input "
       @fields << @field.fetch(attr_name)
       @fields << (options.map { |name, value| " #{name}='#{value}'" })
       @fields << ">\n"
@@ -62,14 +63,14 @@ class Struct
 
   def submit(*button_name)
     @fields << "  <input type='submit'"
-    @fields << " value='#{button_name.present? ? button_name.join : 'Save'}'"
+    @fields << " value='#{button_name.present? ? button_name.join : "Save"}'"
     @fields << ">\n"
     @fields.join
   end
 
   def label(name, *label)
     label << "  <label for='#{name}'"
-    label << '>'
+    label << ">"
     label << name.to_s.capitalize
     label << "</label>\n"
     label.join
