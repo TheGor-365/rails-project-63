14f56888e247239ea52f3d45a9b57ec20639693b
Gor <7495500@gmail.com>
2024-01-24 00:13:46 +0300

    Delete class variable

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -8,15 +8,13 @@ require "active_support/all"
 # generates HTML forms
 module HexletCode
   autoload(:Tag, "./lib/hexlet_code/tag.rb")
-  
+
   def self.form_for(struct, url = {}, *form)
     form << (url.key?(:url) ? "<form action='#{url.fetch(:url)}' method='post'>\n" : "<form action='#' method='post'>\n")
     form << yield(struct)
     form << "</form>"
     form.join
   end
-
-  @@input = []
 end
 
 # generates HTML fields for form
@@ -24,6 +22,8 @@ class Struct
   include HexletCode
 
   def input(key, **options)
+    @input = []
+    
     field = self.to_h.each_with_object({}) do |(name, value), pair|
       pair[name] = case options[:as]
                    when :text then "name='#{name}' cols='#{options.fetch(:cols, 20)}' rows='#{options.fetch(:rows, 40)}'"
@@ -33,26 +33,26 @@ class Struct
 
     case options[:as]
     when :text
-      @@input << label(key)
-      @@input << "  <textarea "
-      @@input << field.fetch(key)
-      @@input << '>'
-      @@input << self.to_h.fetch(key)
-      @@input << "</textarea>\n"
+      @input << label(key)
+      @input << "  <textarea "
+      @input << field.fetch(key)
+      @input << '>'
+      @input << self.to_h.fetch(key)
+      @input << "</textarea>\n"
     else
-      @@input << label(key)
-      @@input << "  <input "
-      @@input << field.fetch(key)
-      @@input << (options.map { |name, value| " #{name}='#{value}'" })
-      @@input << ">\n"
+      @input << label(key)
+      @input << "  <input "
+      @input << field.fetch(key)
+      @input << (options.map { |name, value| " #{name}='#{value}'" })
+      @input << ">\n"
     end
   end
 
   def submit(*name)
-    @@input << "  <input type='submit'"
-    @@input << " name='#{name.any? ? name.join : 'Save'}'"
-    @@input << ">\n"
-    @@input.join
+    @input << "  <input type='submit'"
+    @input << " name='#{name.any? ? name.join : 'Save'}'"
+    @input << ">\n"
+    @input.join
   end
 
   def label(name, *label)
