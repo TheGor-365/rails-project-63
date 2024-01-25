5d09049a07430783800a6ac52ed09ba1c87967ad
Gor <7495500@gmail.com>
2024-01-25 14:50:26 +0300

    Add attr_reader to Struct class

--- lib/hexlet_code.rb
+++ lib/hexlet_code.rb
@@ -24,6 +24,7 @@ end
 # generates HTML fields for form
 class Struct
   include HexletCode
+  attr_reader :input
 
   def input(key, **options)
     public_send(key) unless to_h[key]
@@ -69,3 +70,111 @@ class Struct
     label.join
   end
 end
+
+# User = Struct.new(:name, :job, keyword_init: true)
+# user = User.new(name: 'rob')
+# pp user; puts
+#
+# form_0 = HexletCode.form_for(user) { |f| }
+# # <form action="#" method="post"></form>
+# puts form_0
+#
+# form_0 = HexletCode.form_for(user, url: '/users') { |f| }
+# # <form action="/users" method="post"></form>
+# puts form_0; puts
+
+
+
+
+
+
+User = Struct.new(:name, :job, :gender, keyword_init: true)
+user = User.new(name: 'rob', job: 'hexlet', gender: 'm')
+
+form_1 = HexletCode.form_for(user) do |f|
+  f.input :name
+  f.input :job, as: :text
+  # f.input :gender
+end
+
+# <form action="#" method="post">
+#   <input name="name" type="text" value="rob">
+#   <textarea name="job" cols="20" rows="40">hexlet</textarea>
+# </form>
+
+puts form_1; puts
+
+
+
+form_2 = HexletCode.form_for(user, url: '##') do |f|
+  f.input :name, class: 'user-input'
+  f.input :job
+end
+
+# <form action="#" method="post">
+#   <input name="name" type="text" value="rob" class="user-input">
+#   <input name="job" type="text" value="hexlet">
+# </form>
+
+puts form_2; puts
+
+
+
+form_3 = HexletCode.form_for user, url: '/users' do |f|
+  f.input :job, as: :text, rows: 50, cols: 50
+end
+
+# <form action="#" method="post">
+#   <textarea cols="50" rows="50" name="job">hexlet</textarea>
+# </form>
+
+puts form_3; puts
+
+
+
+
+form_4 = HexletCode.form_for user, url: '/users/path' do |f|
+  f.input :name
+  f.input :job, as: :text
+  # f.input :age
+end
+
+# =>  `public_send': undefined method `age' for #<struct User id=nil, name=nil, job=nil> (NoMethodError)
+
+puts form_4; puts
+
+
+
+form_5 = HexletCode.form_for user do |f|
+  f.input :name
+  f.input :job
+  f.submit
+end
+
+# <form action="#" method="post">
+#   <label for="name">Name</label>
+#   <input name="name" type="text" value="">
+#   <label for="job">Job</label>
+#   <input name="job" type="text" value="hexlet">
+#   <input type="submit" value="Save">
+# </form>
+
+puts form_5; puts
+
+
+
+form_6 = HexletCode.form_for user, url: '#' do |f|
+  f.input :name
+  f.input :job
+  f.submit 'Wow'
+end
+
+# <form action="#" method="post">
+#   <label for="name">Name</label>
+#   <input name="name" type="text" value="">
+#   <label for="job">Job</label>
+#   <input name="job" type="text" value="hexlet">
+#   <input type="submit" value="Wow">
+# </form>
+
+puts form_6; puts
